d3759effe801c961d67685e8a7a502b4c6302d31
RedstoneScratch <31410740+redstone2010@users.noreply.github.com>
2020-06-09 18:19:20 -0400

    Create editor.py

---
+++ python-os/v3/editor.py
@@ -0,0 +1,7 @@
+def editor():
+  print("Welcome to Text Editor")
+  f = None
+  path = None
+  action = input("Do you want to create or open a new file? ('open'/'new')")
+  if action == "open":
+    raise NotImplementedError ("Not implemented yet.")
